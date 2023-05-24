d19bb02c89309bd485dde879be30259e83b13e38
krishna121996 <krishnaprasath00@gmail.com>
2023-05-24 09:28:21 +0530

    Navigation connected

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {HomeView} from './components/Home/HomeView';
 import {CartView} from './components/Cart/CartView';
+import {About} from './components/About/About';
+import {Product} from './components/Products/Product';
+
 import NavBar from  './components/NavBar/NavBar'
 
 const App = () => {
@@ -12,6 +15,8 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<HomeView/>}/>
           <Route exact path="/cart" element={<CartView/>}/>
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/product" element={<Product/>}/>
           <Route></Route>
 
         </Routes>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,15 +3,18 @@ import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div class="flex flex-row-reverse space-x-8 w-100% h-24 space-y-8 bg-red-500">
-        <div>Sign In</div>
-        <div>Products</div>
-        <div>About</div>
-        <div>
+    <div class="flex flex-row-reverse space-x-14 w-100% h-24 space-y-8 bg-red-500 space-x-reverse">
+      <div></div>
+        <div class="text-white">Sign In</div>
+        <div class="text-white">
+        <Link className="Nav__link" to="/about">About</Link>
+        </div>
+        <div class="text-white">
+        <Link className="Nav__link" to="/product">Product</Link>
+        </div>
+        <div class="text-white">
         <Link className="Nav__link" to="/">Home</Link>
-
-            
-            </div>
+        </div>
     </div>
   )
 }
